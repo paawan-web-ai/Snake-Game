c4880f51b5b9737146ca3cc5317173f5e1b79edb
paawan-web-ai <matrixescape345@gmail.com>
2026-01-29 23:12:59 +0530

    adding snake moving feature

--- script.js
+++ script.js
@@ -11,15 +11,9 @@ const blocks = []
 const snake =[
     {
         x:1 , y:3
-    },
-    {
-        x:1 , y:4
-    },
-    {
-        x:1 , y:5
-    },
+    }
 ]
-
+let direction = 'up'
 
 for(let row = 0; row<rows; row++ ){
     for(let col = 0; col<cols; col++){
@@ -37,3 +31,39 @@ function render(){
    const box =blocks[`${val.x}-${val.y}`].classList.add("fill")
    })
 }
+
+setInterval(() => {
+    
+    let head = null
+
+    if(direction==="left"){
+        head = {x:snake[0].x , y:snake[0].y-1} 
+    }else if(direction==="right"){
+        head = {x:snake[0].x , y:snake[0].y+1}
+    }else if(direction==="down"){
+        head ={x:snake[0].x+1 ,y:snake[0].y}
+    }else if(direction==="up"){
+        head ={x:snake[0].x-1 ,y:snake[0].y}
+    }
+    
+    snake.forEach(segment=>{
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
+    })
+
+    snake.unshift(head)
+    snake.pop()
+
+    render()
+}, 400);
+
+addEventListener("keydown",(event)=>{
+    if(event.key === "ArrowUp"){
+        direction="up"
+    }else if(event.key ==="ArrowRight"){
+        direction="right"
+    }else if(event.key ==="ArrowLeft"){
+        direction="left"
+    }else if(event.key ==="ArrowDown"){
+        direction="down"
+    }
+})
